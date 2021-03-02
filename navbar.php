875b62a6f723465f74892cfb2b8c671b9430be09
Mruwawa <52871209+Mruwawa@users.noreply.github.com>
2021-03-02 12:12:33 +0100

    Delete navbar.php

--- navbar.php
+++
@@ -1,7 +0,0 @@
-<div class="navbar">
-    <span class="navbar-item"><a href="index.php" class="navbar-link">Strona główna</a></span>
-    <span class="navbar-item"><a href="animals.php" class="navbar-link">Zwierzęta</a></span>
-    <span class="navbar-item"><a href="map.php" class="navbar-link">Mapa</a></span>
-    <span class="navbar-item"><a href="location.php" class="navbar-link">Lokalizacja</a></span>
-    <span class="navbar-item"><a href="contact.php" class="navbar-link">Kontakt</a></span>
-</div>
